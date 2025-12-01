d431ff0f8318bba04ade630c561fd57babb545f2
pognova <23120322@student.hcmus.edu.vn>
2025-12-01 13:59:45 +0700

    Fix register

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 
 // API Function
 const registerUser = async (data: any) => {
-  const response = await axios.post('https://ia03-userregistrationapiwithreactfrontend.onrender.com', data);
+  const response = await axios.post('https://ia03-userregistrationapiwithreactfrontend.onrender.com/user/register', data);
   return response.data;
 };
 
